65305d4b09872c952eb3017de6c07ece755b73cb
ike barranco <barranco.ike@outlook.com>
2020-07-15 15:49:48 -0600

    added wrapper element page for the app

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-
+import WrapperElement from "./components/WrapperElement.js"
 function App()
 {
     return (
-        <div>
-
-        </div>
+        <WrapperElement />
     )
 }
 
