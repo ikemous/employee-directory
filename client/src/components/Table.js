dfbc6060d48780ba22a634bd447008d2a0436ec3
ike barranco <barranco.ike@outlook.com>
2020-07-17 09:55:34 -0600

    added component navabar and table. Added table to wrapper

---
+++ client/src/components/Table.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Table(props)
+{
+    return (null);
+};
+
+export default Table;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import Form from "../components/Form.js";
+import Wrapper from "../components/Wrapper.js";
+import Table from "../components/Table.js";
 
 
 function Main()
 {
     const [filter, setFilter] = useState("");
-
+    const [results, setResults] = useState("");
+    
     return(
-        <Form filter={filter} setFilter={event => setFilter(event.target.value)}/>
+        <Wrapper>
+            <Form filter={filter} setFilter={event => setFilter(event.target.value)}/>
+            <Table />
+        </Wrapper>
     );
 };
 
