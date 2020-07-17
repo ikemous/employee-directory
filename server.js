abd29d1e03bfaeffae7dd12f5fbe90afd779de22
ike barranco <barranco.ike@outlook.com>
2020-07-17 10:11:06 -0600

    added mongoose to the server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3001;
+const MONGO_URL = process.env.MONGODB_URL || "mongodb://localhost/employees";
 const app = express();
 
+mongoose.connect(MONGO_URL);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
