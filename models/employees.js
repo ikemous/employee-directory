2c3d3eb3c5d712aeb1b9923427a3d1b7ba58708c
ike barranco <barranco.ike@outlook.com>
2020-07-17 10:22:33 -0600

    added employee information to the model employees

--- models/employees.js
+++ models/employees.js
@@ -4,7 +4,49 @@ const moment = require("moment");
 const Schema = mongoose.Schema;
 
 const EmployeeSchema = new Schema(
-
+    {
+        createdDate: {
+            type: String,
+            default: Date.now
+        },
+        updatedDate: {
+            type: String,
+            default: Date.now
+        },
+        firstname: {
+            type: String,
+            trim: true,
+            required: "First Name Is Required"
+        },
+        lastname: {
+            type: String,
+            trim: true,
+            required: "Last Name Is Required"
+        },
+        fullname: {
+            type: String,
+            default: `${this.firstname} ${this.lastname}`
+        },
+        department: {
+            type: String,
+            trim: true,
+            default: "None"
+        },
+        manager: {
+            type: Boolean,
+            default: false
+        },
+        pay: {
+            type: Number,
+            trim: true,
+            default: 0,
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
 );
 
 const EmployeeInformation = mongoose.model("EmployeeInformation", EmployeeSchema);
