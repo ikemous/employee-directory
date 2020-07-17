0020c0ab6dbab5d03c7271db857b2c60793cce96
ike barranco <barranco.ike@outlook.com>
2020-07-17 10:00:12 -0600

    added navbar guts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Navbar()
 {
-    return (null);
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <Link className="navbar-brand" to={"/"}>Employee Directory</Link>
+        </nav>
+    );
 };
 
 export default Navbar;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,15 +2,17 @@ import React, {useState} from "react";
 import Form from "../components/Form.js";
 import Wrapper from "../components/Wrapper.js";
 import Table from "../components/Table.js";
+import Navbar from "../components/Navbar.js";
 
 
 function Main()
 {
     const [filter, setFilter] = useState("");
     const [results, setResults] = useState("");
-    
+
     return(
         <Wrapper>
+            <Navbar />
             <Form filter={filter} setFilter={event => setFilter(event.target.value)}/>
             <Table />
         </Wrapper>
